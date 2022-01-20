710225a49d3031a073da030258958b57a897e350
miten <57693631+m1ten@users.noreply.github.com>
2022-01-20 09:24:09 -0800

    math and avatar

---
+++ src/commands/avatar.ts
@@ -0,0 +1,31 @@
+import { SlashCommandBuilder } from "@discordjs/builders";
+
+export const data = new SlashCommandBuilder()
+	.setName("avatar")
+	.setDescription(
+		"Get the avatar URL of the selected user, or your own avatar."
+	)
+	.addUserOption((option) =>
+		option.setName("user").setDescription("The user's avatar to show")
+	)
+	.addBooleanOption((option) =>
+		option.setName("ephemeral").setDescription("Show or hide message"));
+
+export async function execute(interaction: any) {
+	const user = interaction.options.getUser("user");
+	const ephemeral = interaction.options.getBoolean("ephemeral");
+
+	if (user)
+		return interaction.reply({
+			content: `${user.username}'s avatar: ${user.displayAvatarURL({
+				dynamic: true,
+			})}`,
+			ephemeral: ephemeral,
+		});
+	return interaction.reply({
+		content: `Your avatar: ${interaction.user.displayAvatarURL({
+			dynamic: true,
+		})}`,
+		ephemeral: ephemeral,
+	});
+}
